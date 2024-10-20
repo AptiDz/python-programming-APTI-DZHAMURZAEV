a15f87b886536d3590909a0c0ba2b12324a22ef1
AptiDz <apti.dzhamurzaev@iths.se>
2024-10-20 13:09:59 +0200

    Completed the vg part of the lab 3

--- Labs/Lab3/Lab3.py
+++ Labs/Lab3/Lab3.py
@@ -36,7 +36,7 @@ y_side = np.array([y for _, y in x_y])
 k_, m_ = np.polyfit(x_side, y_side, 1)
     
 
-# Creating a function that takes axes, slope and intercept as argument and determines if the line is above or below in y axes. works only on first degree linear equations.
+# Creating a function that takes axis, slope and intercept as argument and determines if the line is above or below in y axis. works only on first degree linear equations.
 def validate_position_1st_deg(x, y, k, m):
     line_y = k * x + m
     if y > line_y:
@@ -81,7 +81,7 @@ below_x = [x for x, _ in below_list]
 below_y = [y for _, y in below_list]      
 
 
-# Changes the size of the plot figure, Adds dots on with different colors to represent the 2 classes and also different color to show there differences. 
+# Changes the size of the plot figure, Adds dots on with different colors to represent the 2 classes and also different color to show their differences. 
 # Plots the line with costom color and line width and moves the legend to different side to make the dots more visable.
 plt.figure(figsize=(10, 6))  
 plt.scatter(above_x, above_y, color="blue", label="Class 0")
@@ -90,20 +90,20 @@ plt.plot(x_side, k_ * x_side + m_, color ="green", lw=1)
 plt.legend(loc='upper left')
 
 
-# Labeling the axes and title
-plt.xlabel("x-axes")        
-plt.ylabel("y-axes")   
+# Labeling the axis and title
+plt.xlabel("x-axis")        
+plt.ylabel("y-axis")   
 plt.title("Labbelled data")
 
 
-# Variables for the each axes lowest and highest values with increment of 1 and adding those inside ticks to make it more precise visable plt functions. 
+# Variables for the each axis lowest and highest values with increment of 1 and adding those inside ticks to make it more precise visable plt functions. 
 x_values = tuple(range(round(min(x_side)), round(max(x_side))+1))
 y_values = tuple(range(round(min(y_side)), round(max(y_side))+1))
 plt.xticks(x_values)
 plt.yticks(y_values)
 
 
-# Adds the grid and straight lines horizontal and vertical making it easier to see where the dots are in the axes.
+# Adds the grid and straight lines horizontal and vertical making it easier to see where the dots are in the axis.
 plt.grid()
 plt.axhline(0, color="black", lw=0.5)
 plt.axvline(0, color="black", lw=0.5)
